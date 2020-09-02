4ef69be190372d10dc8f053b333cdf9098d533fb
Godspower <godspowereze260@gmail.com>
2020-09-02 12:55:39 +0100

    added the static root

--- localcelery/settings.py
+++ localcelery/settings.py
@@ -85,8 +85,12 @@ ASGI_APPLICATION = 'localcelery.routing.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'caragent',
+        'USER': 'godspower2',
+        'PASSWORD': 'Eminentfablous50',
+        'HOST': 'localhost',
+        'PORT': '5432'
     }
 }
 
